b4c89a52e55c510e03cd1338821ededd0067fac1
lz <lzandljz@163.com>
2026-05-11 10:55:47 +0800

    chore: use production API base URL

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL = 'https://lzljz.top/api'
-export const BASE_URL = 'http://localhost:3000/api'
+export const BASE_URL = 'https://lzljz.top/api'
+// export const BASE_URL = 'http://localhost:3000/api'
 
 export function buildApiUrl(path: string): string {
   return `${BASE_URL}${path}`
